53f819d7ccded505f149b0ac3ded147ca02c717e
alicedes <alice.destrait@outlook.com>
2023-03-21 14:59:18 +0100

    simplegui cleaned

--- SimpleGui.py
+++ SimpleGui.py
@@ -3,9 +3,14 @@ import pygame
 pygame.init()
 
 WINDOWSIZE=(1000,800)
+PURPLE=(186,85,211)
+BLACK=(0,0,0)
+WHITE=(255,255,255)
+
 running=True
 screen= pygame.display.set_mode(WINDOWSIZE)
-PURPLE=(138,43,226)
+pygame.display.set_caption("4x6 Checkers")
+
 
 while running:
     for event in pygame.event.get():
